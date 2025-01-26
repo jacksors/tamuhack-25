7292976c57a8f6d59cfd1919a04942845aa6d24c
Jackson Stone <jackson@jstone.us>
2025-01-26 12:03:10 -0600

    Fix

--- src/app/cars/[id]/page.tsx
+++ src/app/cars/[id]/page.tsx
@@ -3,13 +3,13 @@ import { getCarDetails } from "@/app/actions/cars";
 import { CarDetails } from "@/components/cars/car-details";
 
 interface CarPageProps {
-  params: {
+  params: Promise<{
     id: string;
-  };
+  }>;
 }
 
 export default async function CarPage({ params }: CarPageProps) {
-  const car = await getCarDetails(params.id);
+  const car = await getCarDetails((await params).id);
 
   if (!car) {
     notFound();
